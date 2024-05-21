b849ba9ae28df6465625b306213d03f73030976c
DoHoonKim <dohoon1097819@gmail.com>
2024-05-21 18:16:06 +0900

    Setup cuda_sumcheck crate(set of gpu kernels)

---
+++ cuda_sumcheck/src/lib.rs
@@ -0,0 +1,18 @@
+#![cfg_attr(
+    target_os = "cuda",
+    no_std,
+    feature(register_attr),
+    register_attr(nvvm_internal)
+)]
+
+use cuda_std::*;
+extern crate alloc;
+
+#[kernel]
+pub unsafe fn add(a: &[f32], b: &[f32], c: *mut f32) {
+    let idx = thread::index_1d() as usize;
+    if idx < a.len() {
+        let elem = &mut *c.add(idx);
+        *elem = a[idx] + b[idx];
+    }
+}
