293283b3fbdce33ded0db0dcdb19bc8c590c228e
DoHoonKim8 <dohoon1097819@gmail.com>
2024-06-19 17:54:08 +0000

    Update kernel to support multi thread blocks

--- sumcheck/src/cuda/kernels/multilinear.cu
+++ sumcheck/src/cuda/kernels/multilinear.cu
@@ -3,18 +3,17 @@
 using namespace bb;
 
 extern "C" __global__ void evaluate(fr* coeffs, fr* point, uint8_t num_vars, fr* monomial_evals) {
-    fr coeff = coeffs[threadIdx.x].to_montgomery_form();
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    fr coeff = coeffs[index].to_montgomery_form();
     if (coeff == fr::zero()) {
-        monomial_evals[threadIdx.x] = fr::zero();
+        monomial_evals[index] = fr::zero();
     } else {
-        monomial_evals[threadIdx.x] = coeff;
+        monomial_evals[index] = coeff;
         for (int i = 0; i < num_vars; i++) {
-            if (((threadIdx.x >> i) & 1) == 1) {
-                point[i].self_to_montgomery_form();
-                monomial_evals[threadIdx.x] *= point[i];
+            if (((index >> i) & 1) == 1) {
+                monomial_evals[index] *= point[i];
             }
         }
-        monomial_evals[threadIdx.x].self_from_montgomery_form();
     }
     return;
 }
