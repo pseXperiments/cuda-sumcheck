42100daa9439c0caa95c9c197610fafa94500820
DoHoonKim8 <dohoon1097819@gmail.com>
2024-07-04 06:38:25 +0000

    Add Makefile and silence warnins due to bindgen

--- sumcheck/src/lib.rs
+++ sumcheck/src/lib.rs
@@ -1,3 +1,6 @@
+// silence warnings due to bindgen
+#![allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
+
 use cudarc::driver::{CudaDevice, DeviceRepr, DriverError, LaunchAsync, LaunchConfig};
 use cudarc::nvrtc::Ptx;
 use ff::PrimeField;
