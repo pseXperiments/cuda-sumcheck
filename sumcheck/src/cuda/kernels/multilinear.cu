768d9e6c280e1c8e499e4a6478c42e026f2b2694
DoHoonKim8 <dohoon1097819@gmail.com>
2024-07-30 06:42:54 +0000

    Remove eval_by_coeff

--- sumcheck/src/cuda/kernels/multilinear.cu
+++ sumcheck/src/cuda/kernels/multilinear.cu
@@ -3,43 +3,6 @@
 
 using namespace bb;
 
-extern "C" __global__ void eval_by_coeff(fr* coeffs, fr* point, uint8_t num_vars, fr* monomial_evals) {
-    const int tid = threadIdx.x;
-    const int idx = blockIdx.x * blockDim.x + threadIdx.x;
-
-	auto step_size = 1;
-	int num_threads = blockDim.x >> 1;
-    bool evaluated = false;
-
-	while (num_threads > 0)
-	{
-        if (!evaluated) {
-            fr coeff = coeffs[idx].to_montgomery_form();
-            monomial_evals[idx] = coeff;
-            for (int i = 0; i < num_vars; i++) {
-                monomial_evals[idx] *= (((idx >> i) & 1) ? point[i] : fr::one());
-            }
-            evaluated = true;
-            __syncthreads();
-            continue;
-        }
-
-		if (tid < num_threads) // still alive?
-		{
-			const auto fst = blockIdx.x * blockDim.x + tid * step_size * 2;
-			const auto snd = fst + step_size;
-			monomial_evals[fst] += monomial_evals[snd];
-		}
-
-		step_size <<= 1;
-		num_threads >>= 1;
-        __syncthreads();
-	}
-    if (tid == 0) {
-        monomial_evals[idx].self_from_montgomery_form();
-    }
-}
-
 __device__ fr merge(fr* evals, fr* point, uint8_t point_index, u_int32_t chunk_size) {
     const int start = chunk_size * (blockIdx.x * blockDim.x + threadIdx.x);
     auto step = 2;
